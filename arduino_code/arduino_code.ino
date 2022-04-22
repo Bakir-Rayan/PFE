35a385b03381e1020692eb7bd52a32f2c907537e
rayan <rayan@DESKTOP-KFGP6KT.localdomain>
2022-04-22 16:07:33 +0100

    arduino code - need to add a part for the servo and serial reading

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -4,24 +4,24 @@ int servoPin = 9;
 int servoPin2 = 9;
 Servo servo;
 Servo servo2;
-int angle = 0;  // servo position in degrees
+int angle = 0;  // servo position in degrees //
 
-// serial control
-int val;
+// serial control //
+int val[5];
 
-// Motor AD connections
+// Motor AD connections //
 int enA = 9;
 int in1 = 8;
 int in2 = 7;
-// Motor AG connections
+// Motor AG connections //
 int enB = 3;
 int in3 = 5;
 int in4 = 4;
-// Motor DD connections
+// Motor DD connections //
 int enC = 9;
 int in5 = 8;
 int in6 = 7;
-// Motor DG connections
+// Motor DG connections //
 int enD = 3;
 int in7 = 5;
 int in8 = 4;
@@ -58,16 +58,228 @@ void setup() {
 	digitalWrite(in8, LOW);
 }
 
-
+// need some changes //
 void loop() {
   if (Serial.available() > 0){
-    val = Serial.read();
-    Serial.write("\n");
-    if(val==1){
-      Serial.write("ON");
-      }
-    if(val==0){
-      Serial.write("OFF");
-      }
+    //val = Serial.read(); // change it to table
+    //
+  }
+}
+
+// forward or backward //
+void For_Backward(int speed, int direction) {
+  analogWrite(enA, speed);
+	analogWrite(enB, speed);  
+  analogWrite(enC, speed);
+	analogWrite(enD, speed);
+
+  // go forward
+  if (direction==1) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    digitalWrite(in5, HIGH);
+    digitalWrite(in6, LOW);
+    digitalWrite(in7, HIGH);
+    digitalWrite(in8, LOW);
+  }
+  // go backward
+  if (direction==0) {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    digitalWrite(in5, LOW);
+    digitalWrite(in6, HIGH);
+    digitalWrite(in7, LOW);
+    digitalWrite(in8, HIGH);      
+  }
+}
+
+// left or right //
+void LeftRight(int speed, int direction) {
+  analogWrite(enA, speed);
+	analogWrite(enB, speed);  
+  analogWrite(enC, speed);
+	analogWrite(enD, speed);
+
+  // go left
+  if (direction==1) {
+    // motor AD
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    // motor AG
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    // motor DD
+    digitalWrite(in5, LOW);
+    digitalWrite(in6, HIGH);
+    // motor DG
+    digitalWrite(in7, HIGH);
+    digitalWrite(in8, LOW);
+  }
+  // go right
+  if (direction==0) {
+    // motor AD
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    // motor AG
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    // motor DD
+    digitalWrite(in5, LOW);
+    digitalWrite(in6, HIGH);
+    // motor DG
+    digitalWrite(in7, HIGH);
+    digitalWrite(in8, LOW);      
+  }
+}
+
+// left diagonal "\" forward or backward //
+void LeftDiagonal(int speed, int direction) {
+  analogWrite(enA, speed);
+	analogWrite(enB, 0);  
+  analogWrite(enC, 0);
+	analogWrite(enD, speed);
+
+  // go forward "\"
+  if (direction==1) {
+    // motor AD
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    // motor AG
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    // motor DD
+    digitalWrite(in5, LOW);
+    digitalWrite(in6, LOW);
+    // motor DG
+    digitalWrite(in7, HIGH);
+    digitalWrite(in8, LOW);
+  }
+  // go backward "\"
+  if (direction==0) {
+    // motor AD
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    // motor AG
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    // motor DD
+    digitalWrite(in5, LOW);
+    digitalWrite(in6, LOW);
+    // motor DG
+    digitalWrite(in7, LOW);
+    digitalWrite(in8, HIGH);      
+  }
+}
+
+// right diagonal "/" forward or backward //
+void RightRight(int speed, int direction) {
+  analogWrite(enA, 0);
+	analogWrite(enB, speed);  
+  analogWrite(enC, speed);
+	analogWrite(enD, 0);
+
+  // go forward "/"
+  if (direction==1) {
+    // motor AD
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    // motor AG
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    // motor DD
+    digitalWrite(in5, HIGH);
+    digitalWrite(in6, LOW);
+    // motor DG
+    digitalWrite(in7, LOW);
+    digitalWrite(in8, LOW);
+  }
+  // go backward "/"
+  if (direction==0) {
+    // motor AD
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    // motor AG
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    // motor DD
+    digitalWrite(in5, LOW);
+    digitalWrite(in6, HIGH);
+    // motor DG
+    digitalWrite(in7, LOW);
+    digitalWrite(in8, LOW);      
+  }
+}
+
+// rotate 350° clockwise or anticlockwise
+void Rotation(int speed, int direction) {
+  analogWrite(enA, speed);
+	analogWrite(enB, speed);  
+  analogWrite(enC, speed);
+	analogWrite(enD, speed);
+
+  // go clockwise
+  if (direction==1) {
+    // motor AD
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    // motor AG
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    // motor DD
+    digitalWrite(in5, LOW);
+    digitalWrite(in6, HIGH);
+    // motor DG
+    digitalWrite(in7, HIGH);
+    digitalWrite(in8, LOW);
+  }
+  // go anticlockwise
+  if (direction==0) {
+    // motor AD
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    // motor AG
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    // motor DD
+    digitalWrite(in5, HIGH);
+    digitalWrite(in6, LOW);
+    // motor DG
+    digitalWrite(in7, LOW);
+    digitalWrite(in8, HIGH);      
+  }
+}
+
+// curve depend on the speed of the motors
+void Curve(int speed, int direction) {
+  analogWrite(enA, speed);
+	analogWrite(enB, speed);  
+  analogWrite(enC, speed);
+	analogWrite(enD, speed);
+
+  // go forward left or right 
+  if (direction==1) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    digitalWrite(in5, HIGH);
+    digitalWrite(in6, LOW);
+    digitalWrite(in7, HIGH);
+    digitalWrite(in8, LOW);
+  }
+  // go backward left or right
+  if (direction==0) {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    digitalWrite(in5, LOW);
+    digitalWrite(in6, HIGH);
+    digitalWrite(in7, LOW);
+    digitalWrite(in8, HIGH);      
   }
 }
