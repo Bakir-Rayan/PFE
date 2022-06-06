f3b280be2c06e722fc186d36f59199643d782f8e
rayan <rayanbak257@gmail.com>
2022-06-06 19:13:23 +0100

    changing from arduino uno to esp32

--- esp32_code/esp32_code.ino
+++ esp32_code/esp32_code.ino
@@ -6,26 +6,56 @@ struct __attribute__((packed)) STRUCT {
   long z[3];
 } testStruct;
 
+volatile long encoderValue1 = 0;
+volatile long encoderValue2 = 0;
+volatile long encoderValue3 = 0;
+volatile long encoderValue4 = 0;
+
+int ENCODEROUTPUT = 11;
+int rpm1 = 0;
+int rpm2 = 0;
+int rpm3 = 0;
+int rpm4 = 0;
+
+int interval = 1000;
+long previousMillis = 0;
+long currentMillis = 0;
+
+int enco1 = 17;
+int enco2 = 5;
+int enco3 = 18;
+int enco4 = 19;
+
 // Motor AD connections //
-int enA = 10;
-int in1 = A1;
-int in2 = A0;
+int enA = 16;
+int in1 = 4;
+int in2 = 2;
 // Motor AG connections //
-int enB = 3;
-int in3 = 2;
-int in4 = 4;
+int enB = 15;
+int in3 = 32;
+int in4 = 33;
 // Motor DD connections //
-int enC = 11;
-int in5 = 13;
-int in6 = 12;
+int enC = 13;
+int in5 = 12;
+int in6 = 14;
 // Motor DG connections //
-int enD = 6;
-int in7 = 8;
-int in8 = 7;
+int enD = 25;
+int in7 = 27;
+int in8 = 26;
 
 void setup() {
   Serial.begin(115200);
   myTransfer.begin(Serial);
+
+  pinMode(enco1, INPUT_PULLUP);
+  pinMode(enco2, INPUT_PULLUP);
+  pinMode(enco3, INPUT_PULLUP);
+  pinMode(enco4, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(enco1), updateEncoder1, RISING);
+  attachInterrupt(digitalPinToInterrupt(enco2), updateEncoder2, RISING);
+  attachInterrupt(digitalPinToInterrupt(enco3), updateEncoder3, RISING);
+  attachInterrupt(digitalPinToInterrupt(enco4), updateEncoder4, RISING);
 
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
@@ -61,23 +91,61 @@ void loop() {
   {
     uint16_t recSize = 0;
     recSize = myTransfer.rxObj(testStruct, recSize);
+    encoderValue1 = 0;
+    encoderValue2 = 0;
+    encoderValue3 = 0;
+    encoderValue4 = 0;
+    previousMillis = millis();    
   }
   int val=testStruct.z[2];
 
-  switch (val) {
-    case 1:
-      For_Backward(testStruct.z[0], testStruct.z[1]);  
-      break;  
-    case 2:
-      Rotation(testStruct.z[0], testStruct.z[1]);
-      break;
-    case 3:
-      Curve(testStruct.z[0], testStruct.z[1]);
-      break;
-    case 4:
-      Stop();
-      break;
-  }  
+  currentMillis = millis();
+  if (currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;    
+    switch (val) {
+      case 1:
+        For_Backward(testStruct.z[0], testStruct.z[1]);  
+        break;  
+      case 2:
+        Rotation(testStruct.z[0], testStruct.z[1]);
+        break;
+      case 3:
+        Curve(testStruct.z[0], testStruct.z[1]);
+        break;
+      case 4:
+        Stop();
+        break;
+    }
+    rpm1 = (float)(encoderValue1 * 60 / ENCODEROUTPUT);
+    rpm2 = (float)(encoderValue2 * 60 / ENCODEROUTPUT);
+    rpm3 = (float)(encoderValue3 * 60 / ENCODEROUTPUT);
+    rpm4 = (float)(encoderValue4 * 60 / ENCODEROUTPUT);
+    Serial.print(rpm);
+    Serial.println(" RPM 1");
+    Serial.print(rpm);
+    Serial.println(" RPM 2");
+    Serial.print(rpm);
+    Serial.println(" RPM 3");
+    Serial.print(rpm);
+    Serial.println(" RPM 4");
+    encoderValue1 = 0;
+    encoderValue2 = 0;
+    encoderValue3 = 0;
+    encoderValue4 = 0;
+  }    
+}
+
+void updateEncoder1() {
+  encoderValue1++;
+}
+void updateEncoder2() {
+  encoderValue2++;
+}
+void updateEncoder3() {
+  encoderValue3++;
+}
+void updateEncoder4() {
+  encoderValue4++;
 }
 
 void For_Backward(int speed, int direction)/* 1 */ {
